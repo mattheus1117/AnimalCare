c975983bbe133baa0db469d30d9bab7c8deddc76
Douglas <126362361+DouglasSLopes@users.noreply.github.com>
2025-05-05 11:43:43 -0300

    Organizing folders

---
+++ src/assets/components/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer(){
+    return (
+        <>
+            <div>
+                <h1>Olá</h1>
+            </div>
+        </>
+    )
+}
---
+++ src/assets/components/header.tsx
@@ -0,0 +1,27 @@
+export function Header(){
+    return (
+        <>
+            <div>
+                <h1>Olá</h1>
+            </div>
+        </>
+    )
+}
+
+// Exemplo de como puxar no App.tsx
+// import "./App.css";
+
+// import { Header } from "./components/header";
+
+// export function App() {
+
+//   return (
+//     <>
+//       <p>Hello World</p>
+//       <div>
+//         <p>Hello World dnv</p>
+//       </div>
+//       <Header /> {/* Forma correta de chamar o componente */}
+//     </>
+//   );
+// }
