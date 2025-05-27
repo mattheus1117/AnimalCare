e2c552ad041ca1ef8d032615fcdd0b9363fb6fec
Matheus Moura <mathmo.ura242@gmail.com>
2025-05-27 12:16:42 -0300

    Instalando tailwind

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Chat from "../components/Chat/Chat";
+import { Link } from "react-router-dom";
 
 export const Login = () => {
     const [chatVisivel, setChatVisivel] = React.useState(false);
 
     return <>
         <Header onChatClick={() => setChatVisivel((v) => !v)} />
-            <h1>LOGIN</h1>
-            <br/>
-            <br/>
-            <br/>Espaço placeholder
-            <br/>
-            <br/>
+        
+        <div className="p-8">
+            <h2>Acesse sua conta</h2>
+            <input type="text" placeholder="Email ou CPF"/>
+            <input type="password" placeholder="Digite sua senha"/>
+            <Link to="/">Esqueci minha senha</Link>
+            <button>Entrar</button>
+        </div>
+
         <Footer />
         {chatVisivel && <Chat onClose={() => setChatVisivel(false)} />}
     </>
